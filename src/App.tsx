fb98f41ecfd5a0006f5755523a74e8419c04c10c
RafaelOliveira7 <rafael.oliveira@estudante.ufscar.br>
2023-09-01 02:23:16 -0300

    Creating integration for google translate

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { FormControl, InputLabel, MenuItem, Select, TextField, Button } from '@mui/material';
 
 function App() {
+  const [sourceLanguage, setSourceLanguage] = useState('');
+  const [targetLanguage, setTargetLanguage] = useState('');
+  const [inputText, setInputText] = useState('');
+  const [translatedText, setTranslatedText] = useState('');
+
+  const handleTranslate = async () => {
+    try {
+      const response = await fetch(
+        `https://translation.googleapis.com/language/translate/v2?key=AIzaSyDPJI6giDF2c4zNXZWW59gGKWJuUrRfZ4c&q=${encodeURIComponent(
+          inputText
+        )}&source=${sourceLanguage}&target=${targetLanguage}`,
+        {
+          method: 'POST',
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        const translation = data.data.translations[0].translatedText;
+        setTranslatedText(translation);
+      } else {
+        console.error('Erro na tradução');
+      }
+    } catch (error) {
+      console.error('Erro na tradução:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormControl>
+        <InputLabel>Idioma de Origem</InputLabel>
+        <Select value={sourceLanguage} onChange={(e) => setSourceLanguage(e.target.value as string)}>
+          <MenuItem value="en">Inglês</MenuItem>
+          <MenuItem value="es">Espanhol</MenuItem>
+          {/* Adicione mais opções de idioma conforme necessário */}
+        </Select>
+      </FormControl>
+      <FormControl>
+        <InputLabel>Idioma de Destino</InputLabel>
+        <Select value={targetLanguage} onChange={(e) => setTargetLanguage(e.target.value as string)}>
+          <MenuItem value="pt">Português</MenuItem>
+          <MenuItem value="fr">Francês</MenuItem>
+          {/* Adicione mais opções de idioma conforme necessário */}
+        </Select>
+      </FormControl>
+      <TextField
+        label="Texto a ser Traduzido"
+        multiline
+        rows={4}
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <Button variant="contained" onClick={handleTranslate}>
+        Traduzir
+      </Button>
+      <TextField
+        label="Texto Traduzido"
+        multiline
+        rows={4}
+        value={translatedText}
+        InputProps={{ readOnly: true }}
+      />
     </div>
   );
 }
